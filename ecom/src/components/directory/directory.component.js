9db93c0a12e2fce3f4ffbcb6a14c8d507c90e68e
chrisshepard <43590660+chrisshepard@users.noreply.github.com>
2019-10-29 23:34:41 -0400

    initial

---
+++ ecom/src/components/directory/directory.component.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './directory.styles.scss';
+import sections from './directory.data';
+import MenuItem from '../menu-item/menu-item.component';
+
+class Directory extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sections: sections
+    }
+  }
+
+  render() {
+    return (
+      <div className="directory-menu">
+        {
+          this.state.sections.map(({id, ...otherSectionProps}) => (
+            <MenuItem key={id} {...otherSectionProps} />
+          ))
+        }
+      </div>
+    )
+  }
+}
+
+export default Directory;
